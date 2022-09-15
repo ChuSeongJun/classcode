7d76c5e851b42b89441c2156fd3b09aab949c6e8
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-15 19:38:40 +0900

    ss

--- Webdb/3주차/prime-worker.js
+++ Webdb/3주차/prime-worker.js
@@ -8,7 +8,7 @@ const {
 const min = 2;
 let primes = [];
 
-function findPrimes(start, range) {
+function findPrimes(start, range) {  //에라토스테네스의 체 알고리즘으로 소수찾기
   let isPrime = true;
   let end = start + range;
   for (let i = start; i < end; i++) {
@@ -27,36 +27,37 @@ function findPrimes(start, range) {
 
 if (isMainThread) {
   const max = 10000000;
-  const threadCount = 8;
+  const threadCount = 8; //8개의 쓰레드 생산할것이다
   const threads = new Set();
-  const range = Math.ceil((max - min) / threadCount);
+  const range = Math.ceil((max - min) / threadCount); //2에서 천만까지 8개의 쓰레드에게 범위 할당
   let start = min;
-  console.time("prime");
-  for (let i = 0; i < threadCount - 1; i++) {
+  console.time("prime"); //시작
+  for (let i = 0; i < threadCount - 1; i++) { // 1번 쓰레드부터 7번까지 반복
     const wStart = start;
     threads.add(
       new Worker(__filename, { workerData: { start: wStart, range } })
+        //각각 쓰레드에 시작값과 끝값이 들어가면서 각자의 쓰레드 초기데이터에 범위를 준다.
     );
     start += range;
   }
-  threads.add(
+  threads.add( //마지막 쓰레드
     new Worker(__filename, {
       workerData: { start, range: range + ((max - min + 1) % threadCount) },
     })
   );
   for (let worker of threads) {
-    worker.on("error", (err) => {
+    worker.on("error", (err) => { //만약 에러가 뜨면 대처 방법
       throw err;
     });
     worker.on("exit", () => {
-      threads.delete(worker);
-      if (threads.size === 0) {
-        console.timeEnd("prime");
-        console.log(primes.length);
+      threads.delete(worker); //쓰레드 한개 끝날때마다 삭제
+      if (threads.size === 0) {  //사이즈가 0이면 모든 일이 끝났다.
+        console.timeEnd("prime"); //끝
+        console.log(primes.length); //시작과 끝의 사이에 얼마나 걸렸나
       }
     });
-    worker.on("message", (msg) => {
-      primes = primes.concat(msg);
+    worker.on("message", (msg) => { //메세지는 쓰레드들이 구한 소수값
+      primes = primes.concat(msg); //8개의 쓰레드가 구한 값을 합쳐준다.
     });
   }
 } else {
--- Webdb/3주차/worker_data.js
+++ Webdb/3주차/worker_data.js
@@ -6,29 +6,31 @@ const {
 } = require("worker_threads");
 
 if (isMainThread) {
-  // 부모일 때
+  // 메인 쓰레드
   const threads = new Set();
+  //Set은 배열인데 중복되지 않는 배열
   threads.add(
     new Worker(__filename, {
-      workerData: { start: 1 },
+      workerData: { start: 1 }, //초기 데이터
     })
   );
   threads.add(
     new Worker(__filename, {
-      workerData: { start: 2 },
+      workerData: { start: 2 },//초기 데이터
     })
   );
   for (let worker of threads) {
     worker.on("message", (message) => console.log("from worker", message));
     worker.on("exit", () => {
-      threads.delete(worker);
-      if (threads.size === 0) {
+      threads.delete(worker); //워커가 끝났을때 삭제 (한개 끝날때마다 삭제)
+      //끝났을때 Set에서 삭제하기 위함
+      if (threads.size === 0) { //모든 워커가 종료됐음을 의미(일이 끝남)
         console.log("job done");
       }
     });
   }
 } else {
   // 워커일 때
-  const data = workerData;
-  parentPort.postMessage(data.start + 100);
+  const data = workerData; //워커데이터에 보냈던 데이터를 받아온다.
+  parentPort.postMessage(data.start + 100); //객체를 보내서 data가 객체가 된다.
 }
