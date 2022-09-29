b2a07395debba9cdbd73a712f16a57b30f261015
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-09-29 17:14:37 +0900

    FEat: 수업 시간에 배운낸용

--- mobileweb/study/src/index.js
+++ mobileweb/study/src/index.js
@@ -5,12 +5,23 @@ import Counter from "./chapter_06/Counter";
 import GoodBad from "./chapter_06/GoodBad";
 import RealtimeText from "./chapter_06/RealtimeText";
 import NotificationList from "./chapter_06/NotificationList";
+import YourCounter from "./chapter_07/YourCounter";
+import Accommodate from "./chapter_07/Accommodate";
+import ToggleTimer from "./chapter_07/ToggleTimer";
+import MyuseRef from "./chapter_07/MyuseRef";
+import MyuseRef2 from "./chapter_07/MyuseRef2";
+import MemoTest from "./chapter_07/MemoTest";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<GoodBad />*/}
     {/*<RealtimeText />*/}
-    <NotificationList />
+    {/*<NotificationList />*/}
+    {/*<YourCounter />*/}
+    {/*<Accommodate />*/}
+    {/*<ToggleTimer />*/}
+    {/*<MyuseRef2 />*/}
+    <MemoTest />
   </React.StrictMode>
 );
