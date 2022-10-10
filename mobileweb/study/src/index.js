fb8e9c480ea6739c852cb801a397c740360eb0b8
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-11 01:45:20 +0900

    Feat: 과제중

--- mobileweb/study/src/index.js
+++ mobileweb/study/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import ToDoList from "./homework4/ToDoList";
+import ToDoList2 from "./use/ToDoList2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoList2 />
   </React.StrictMode>
 );
