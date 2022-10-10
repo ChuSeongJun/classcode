49cff8bfcbda3a2613077290f21df34c97eb5fac
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-10-11 00:31:27 +0900

    Feat: 과제중

---
+++ mobileweb/study/src/homework4/ToDoList.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import "./ToDoList.css";
+function ToDoList() {
+  const [inputItem, setInputItem] = useState("");
+  const [itemList, setItemList] = useState([
+    { id: "1", value: "리액트듣기" },
+    { id: "2", value: "점심먹기" },
+    { id: "3", value: "과제하기" },
+  ]);
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+  const [localcontent, setLocalContent] = useState("");
+
+  const AddToDoList = () => {
+    setItemList((prevItem) => {
+      console.log(itemList.length + 1, inputItem);
+      return [
+        {
+          id: itemList.length + 1,
+          value: inputItem,
+        },
+        ...prevItem,
+      ];
+    });
+    setInputItem("");
+  };
+
+  const DeleteToDoList = (id) => {
+    setItemList((itemList) =>
+      itemList.filter((itemList) => itemList.id !== id)
+    ); // 키가 중요하게 사용되는 순간!!
+    console.log(itemList);
+  };
+
+  return (
+    <div className="container">
+      <h1> To-Do List </h1>
+      <div className="add-elements">
+        <input
+          id="add"
+          value={inputItem}
+          onChange={(event) => setInputItem(event.target.value)}
+        />
+        <button id="btn" onClick={AddToDoList}>
+          할일추가
+        </button>
+      </div>
+      <div id="element-list">
+        <ul id="list">
+          {itemList.map((item) => {
+            return (
+              <li key={item.id}>
+                {isEdit ? (
+                  <>
+                    <textarea
+                      value={localcontent}
+                      onChange={(e) => setLocalContent(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <>{item.value}</>
+                )}
+                {isEdit ? (
+                  <>
+                    <button onClick={toggleIsEdit}>수정 취소</button>
+                    <button onClick={toggleIsEdit}>수정 완료</button>
+                  </>
+                ) : (
+                  <></>
+                )}
+                <img
+                  src="trash.jpg"
+                  onClick={() => DeleteToDoList(item.id)}
+                  alt=""
+                ></img>
+                <img src="pen.jpg" onClick={toggleIsEdit} alt=""></img>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default ToDoList;
--- mobileweb/study/src/index.js
+++ mobileweb/study/src/index.js
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Counter from "./chapter_06/Counter";
-import GoodBad from "./chapter_06/GoodBad";
-import RealtimeText from "./chapter_06/RealtimeText";
-import NotificationList from "./chapter_06/NotificationList";
-import AirbnbList from "./homework/AirbnbList";
-import MovieList from "./homework2/MovieList";
-import Toggle from "./homework3/Toggle";
-import EventParam from "./homework3/EventParam";
-import MyLotto from "./homework3/MyLotto";
-import LoginControl from "./homework3/LoginControl";
+import ToDoList from "./homework4/ToDoList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/*<GoodBad />*/}
-    {/*<RealtimeText />*/}
-    {/*<NotificationList />*/}
-
-    {/*<AirbnbList />*/}
-    {/*AirbnbList 불러오기*/}
-    {/*<MovieList />*/}
-    {/*MovieList불러오기*/}
-    {/*  <Toggle/>*/}
-    {/*  <EventParam/>*/}
-    <MyLotto />
-    {/*  <LoginControl/>*/}
+    <ToDoList />
   </React.StrictMode>
 );
