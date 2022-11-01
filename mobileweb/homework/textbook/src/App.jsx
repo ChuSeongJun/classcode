dfd1313679c27d0c899fdef96a9eb090678bfe20
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-11-02 00:28:12 +0900

    Feat: 9-2 회원확인 과제 완료

--- mobileweb/homework/textbook/src/App.jsx
+++ mobileweb/homework/textbook/src/App.jsx
@@ -3,27 +3,52 @@ import "./App.css";
 const serverURL = "http://localhost:65020/users";
 function App() {
   const [userData, setUserData] = useState(null);
+  const [checkbtn, setCheckBtn] = useState(false);
+  const [checkData, setCheckData] = useState(false);
+
   const getUserData = () => {
-    fetch(serverURL) // fetch함수를 이용해 REST API로 회원목록을 요청
-      .then((res) => res.json()) // 회원목록을 json포맷으로 수신
-      .then((data) => setUserData(data)) // 받은 데이터를 setState 함수로 업데이트 함.
-      .then(console.log(userData)); // 콘솔창에 출력해서 확인함
+    fetch(serverURL)
+      .then((res) => res.json())
+      .then((data) => setUserData(data))
+      .then(console.log(userData));
   };
-  useEffect(getUserData, []); // mount시에만 서버데이터를 가져오도록 이펙트처리
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const id = event.target.id.value;
-    const passwd = event.target.passwd.value;
+  useEffect(getUserData, []);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const id = e.target.id.value;
+    const passwd = e.target.passwd.value;
     console.log("Submit버튼 클릭후 서버로 POST전송");
     fetch(serverURL, {
-      // fetch함수로 데이터를 서버로 전송!
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name, id, passwd }),
-    }).then(getUserData()); // 등록한 데이터를 다시 가져옴
+    }).then(getUserData());
+  };
+  const onSubmitClick = (e) => {
+    //버튼을 눌렀을때 실행되는 이벤트
+    e.preventDefault();
+
+    setCheckBtn(true);
+
+    const id = e.target.id.value;
+    const passwd = e.target.passwd.value;
+
+    console.log(id);
+    console.log(passwd);
+
+    //map을 사용해 안에 들어있는 값을 꺼내서 비교한다.
+    userData.map((i, index) => {
+      if (id === i.id && passwd === i.passwd) {
+        setCheckData(true);
+        //참이면 true로 바뀌어서 에러메시지출력
+      } else {
+        setCheckData(false);
+        //거짓이면 false로 바뀌어서 에러메시지출력
+      }
+    });
   };
 
   return (
@@ -40,12 +65,24 @@ function App() {
       <p></p>
       <div>
         <h2>회원확인</h2>
-        <form onSubmit={onSubmitHandler}>
+        <form onSubmit={onSubmitClick}>
           <input type="text" name="id" placeholder="아이디" />
           <input type="text" name="passwd" placeholder="암호" />
           <button type="submit">확인</button>
         </form>
       </div>
+      {/*체크 버튼 누르기전까지는 false라 안보이고 누르면 상태 바뀌어서 보임*/}
+      {checkbtn && (
+        <div className="error">
+          {/*클래스네임 error로 스타일 설정*/}
+          {/*checkData가 참이면 회원으로 확인 거짓이면 그런회원은 없습니다를 출력하는 삼항연산자*/}
+          {checkData ? (
+            <div>회원으로 확인되었습니다.</div>
+          ) : (
+            <div>그런 회원은 없습니다.</div>
+          )}
+        </div>
+      )}
       <p></p>
       <div>
         <h2>회원명단</h2>
